c0874e26aa5670b0329d6a1d1bbc23e6b116f8f3
Galen Seilis <galen.seilis@seilis.ca>
2023-10-17 12:42:15 -0700

    Update distributions.py
    Added deterministic sequence with naive extrapolation heuristic.

--- distributions.py
+++ distributions.py
@@ -10,7 +10,7 @@ class SeqPMFNaive(cw.distributions.Distribution):
 	These naive extrapolation assumptions are not intended
 	'''
 	def __init__(self, distseq):
-		if not all(map(lambda o: isinstance(o, ciw.dists.Pmf))):
+		if not all(map(lambda o: isinstance(o, ciw.dists.Pmf)), distseq):
 			raise ValueError('Not all inputs of distseq were of type ciw.dists.Pmf')
 		self.distseq = distdeq
 
@@ -24,3 +24,22 @@ class SeqPMFNaive(cw.distributions.Distribution):
 			return self.distseq[min(self.distseq)].sample(time, ind)
 		else:# TODO: Linearly interpolate probabilities wrt t
 			raise ValueError(f'Unsupported sampling time of {t} in SeqPMFNaive.')
+
+class DeterministicSeqNaive(cw.distributions.Distribution):
+	'''Samples from a sequence of constant random variables.'''
+	def __init__(self, distseq):
+		if not all(map(lambda o: isinstance(o, ciw.dists.Deterministic)), distseq):
+			raise ValueError("Not all inputs of distseq were of type ciw.dists.Deterministic")
+		self.distseq = distseq
+
+	def sample(self, t, ind=None):
+		time = round(t)
+		if time in self.disteq:
+			return self.distseq[time].sample(time, ind)
+		elif time > max(self.distseq):
+			return self.distseq[max(self.distseq)].sample(time, ind)
+		elif time < min(self.distseq)::
+			return self.distseq[min(self.distseq)].sample(time, ind)
+		else:
+			raise ValueError(f'Unsupported sampling time of {t} in DeterministicSeqNaive.')
+			
