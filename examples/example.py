52841055febd0ce74af119d964883e0df3a6ecde
galenseilis <galen.seilis@seilis.ca>
2024-07-02 19:34:51 -0700

    mvp server optimizer

--- examples/example.py
+++ examples/example.py
@@ -1,11 +1,14 @@
+from copy import copy
 from itertools import product
 from typing import Callable
+
+import ciw
 
 
 class ServerOptimizer:
     """Simulation optmizer."""
 
-    def __init__(self, ciw_network, obj_func: Callable):
+    def __init__(self, ciw_network: ciw.Network, obj_func: Callable):
         """
         Args:
             ciw_network (ciw.Network): Ciw network to be optimized.
@@ -27,12 +30,28 @@ class ServerOptimizer:
 
         Args:
             reps (int): Number of replicates to run.
+
+        Returns:
+            best_score, best_allocation
         """
+        best_score = None
+        best_allocation = None
+        sim_kwargs = {} if sim_kwargs is None else sim_kwargs
         for server_allocation in product(*server_grid):
             N = self._gen_new_ciw_network(server_allocation)
             for replicate in range(reps):
                 S = ciw.Simulation(N, **sim_kwargs)
                 S.simulate_until_max_time(sim_time)
+                score = self.obj_func(S)
+                if best_score is None:
+                    best_score = score
+                    best_allocation = server_allocation
+                elif score < best_score:
+                    best_score = score
+                    best_allocation = server_allocation
+                else:
+                    continue
+        return best_score, best_allocation
 
     def _gen_new_ciw_network(self, server_alloc: list[int]):
         """Create copy of network with new server allocation.
@@ -43,7 +62,31 @@ class ServerOptimizer:
         The length of server_alloc must be the same as the length
         of the Ciw network's service_centres attribute.
         """
-        N = self.ciw_network.copy()
-        for sc, s in zip(*N.service_centres, server_alloc):
+        N = copy(self.ciw_network)
+        if len(N.service_centres) != len(server_alloc):
+            raise ValueError(
+                "Number of server allocations does not match the number of service centres."
+            )
+        for sc, s in zip(N.service_centres, server_alloc):
             sc.number_of_servers = s
         return N
+
+
+if __name__ == "__main__":
+    N = ciw.create_network(
+        arrival_distributions=[ciw.dists.Exponential(rate=0.2)],
+        service_distributions=[ciw.dists.Exponential(rate=0.1)],
+        number_of_servers=[3],
+    )
+
+    server_grid = [list(range(1, 10))]
+
+    def objective(sim: ciw.Simulation):
+        num_servers = [sc.number_of_servers for sc in sim.network.service_centres]
+        num_servers = tuple(num_servers)
+        neg_completed = -len(sim.get_all_records())
+        return (neg_completed,) + num_servers
+
+    SO = ServerOptimizer(N, objective)
+
+    print(SO.run(1440, server_grid, reps=10))
